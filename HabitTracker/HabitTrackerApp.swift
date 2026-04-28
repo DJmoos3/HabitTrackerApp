0a7a5ae348a46337d8d2760a3ddf32bdab9f52d5
DJmoos3 <isaac.strandh@gmail.com>
2026-04-28 20:45:08 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Isaac Strandh on 2026-04-28.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
